6a0b01f1bf35d4cf63db283c83e5d2684a11ce1a
danushka181 <danushka181@gmail.com>
2023-04-14 06:57:34 +0530

    option data save and update

--- App/Data/DmSettingOptionHandler.php
+++ App/Data/DmSettingOptionHandler.php
@@ -12,8 +12,12 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-class DmOptionHandler {
+use Ddev\Traits\DmSettingsOptionTraits;
+
+class DmSettingOptionHandler {
+
     use DmSettingsOptionTraits;
+
     /**
      * Option name for save data in database.
      *
@@ -26,9 +30,14 @@ class DmOptionHandler {
      * Create Setting options in the Database.
      * @return void
      */
-    public function create_setting_option() {
+    public function create_setting_option(): void
+    {
         //check settings already saved.
-
+        $settings = $this->get_saved_dm_option_settings( $this->option_name );
+        if ( ! $settings ) {
+            $value = $this->get_default_settings();
+            $dd = $this->save_dm_option_settings_to_database( $this->option_name, $value );
+        }
     }
 
 
