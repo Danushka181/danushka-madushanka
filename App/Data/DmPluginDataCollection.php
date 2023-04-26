259eda346d7f3ae6082383a8b01520b3c38a152a
DanushkaDdev <danushka181@gmail.com>
2023-04-27 00:09:50 +0530

    change return types

--- App/Data/DmPluginDataCollection.php
+++ App/Data/DmPluginDataCollection.php
@@ -32,7 +32,7 @@ class DmPluginDataCollection {
 	 *
 	 * @var string
 	 */
-	private string $cache_key = '';
+	private string $cache_key;
 
 	/**
 	 * Set cache key.
@@ -50,11 +50,15 @@ class DmPluginDataCollection {
 	 * @since 1.0.0
 	 */
 	public function process_plugin_api_data(): void {
-		$cache_data = $this->load_cached_data_by_cache_key();
+		$cache_data = $this->load_cache_data();
 		if ( false === $cache_data ) {
 			$this->call_to_dm_api_service();
 		}
 		new DmScheduleApiCall();
+	}
+
+	public function load_cache_data(): mixed {
+		return $this->load_cached_data_by_cache_key();
 	}
 
 	/**
@@ -73,15 +77,15 @@ class DmPluginDataCollection {
 	/**
 	 * Request data From server.
 	 *
-	 * @return bool|array
+	 * @return string|array
 	 * @since 1.0.0
 	 */
-	public function dm_request_data_from_server(): bool|array {
+	public function dm_request_data_from_server(): string|array {
 		$api_service = new DmApiService( $this->api_link );
 		$api_data = $api_service->fetch_data_from_server();
 		if ( $api_data ) {
-			return json_encode( $api_data );
+			return $api_data;
 		}
-		return false;
+		return [];
 	}
 }
